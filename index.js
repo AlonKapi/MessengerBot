e7c2f242b64d81534f98a3b5fefe346bcfcadcf7
Alon Kaplanski <alon.k@Taptica.local>
2018-08-16 22:18:14 +0300

    adding basic bot response

--- index.js
+++ index.js
@@ -26,9 +26,16 @@ app.post('/webhook', (req, res) =>{
             console.log(webhook_event);
             
             // Get the sender PSID
-            console.log('HERE???');
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the event is a message or a postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message){
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback){
+                handlePostback(sender_psid, webhook_event.postback);
+            }
         });
 
         // returns a 'OK' response to all requests
@@ -64,7 +71,18 @@ app.get('/webhook', (req, res) => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
+    let response;
 
+    // Check if the message contains text
+    if (received_message.text){
+        // Create the payload for a basic text message
+        response = {
+            "text" : 'You sent the message: "${received_message.text}". Now send me an image!'
+        }
+    }
+
+    // Sends the response message
+    callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postbacks events
@@ -74,5 +92,11 @@ function handlePostback(sender_psid, received_postback) {
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-  
+  // Construct the message body
+  let request_body = {
+      "recipient": {
+          "id": sender_psid
+      },
+      "message": response
+  }
 }
